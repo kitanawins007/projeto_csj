827192b0e411a1e4d42503135deb59ff3f750d9a
Lenovo <Lenovo@DESKTOP-J07EOB4>
2024-02-19 21:46:16 -0300

    Adicionar arquivos de projeto.

---
+++ conclusão_csj/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Floricultura
+{
+    class Jardim
+    {
+        static void Main(string[] args)
+        {
+            int florzinha = Convert.ToInt32(Console.ReadLine());
+            
+                switch (florzinha)
+                {
+                    case 0: Console.WriteLine("Margarida");break;
+                    case 1: Console.WriteLine("Girassol");break;
+                    case 2: Console.WriteLine("Lavanda");break;
+                }
+
+            if (florzinha > 2)
+            {
+                Console.WriteLine("Margarida+Girassol+Lavanda");
+            }
+
+            
+        }
+    }
+}
+
